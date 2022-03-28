3974aa35ac2bc72161e680f98b21a4deaa99f48b
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-03-28 15:34:31 +0200

    configure receive and transmitter

--- lib/usrp.cpp
+++ lib/usrp.cpp
@@ -3,8 +3,21 @@
 ErrorCode Usrp::setRfConfig(const RfConfig& conf) {
     // init usrp
     ErrorCode retCode = SUCCESS;
-    usrp_->set_tx_rate(conf.txSamplingRate);
-    usrp_->set_rx_rate(conf.rxSamplingRate);
 
+    // configure transmitter
+    usrp_->set_tx_rate(conf.txSamplingRate);
+    usrp_->set_tx_subdev_spec(uhd::usrp::subdev_spec_t("A:0"), 0);
+    uhd::tune_request_t txTuneRequest(conf.txCarrierFrequency);
+    usrp_->set_tx_freq(txTuneRequest, 0);
+    usrp_->set_tx_gain(conf.txGain, 0);
+    usrp_->set_tx_bandwidth(conf.txAnalogFilterBw, 0);
+
+    // configure receiver
+    usrp_->set_rx_rate(conf.rxSamplingRate);
+    usrp_->set_rx_subdev_spec(uhd::usrp::subdev_spec_t("A:0"), 0);
+    uhd::tune_request_t rxTuneRequest(conf.rxCarrierFrequency);
+    usrp_->set_rx_freq(rxTuneRequest, 0);
+    usrp_->set_rx_gain(conf.rxGain, 0);
+    usrp_->set_rx_bandwidth(conf.rxAnalogFilterBw, 0);
     return retCode;
 }

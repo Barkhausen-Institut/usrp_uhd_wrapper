8ec939b5d5ef51f690b560e243560158415fed39
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-03-29 11:01:24 +0200

    add snippets folder

---
+++ snippets/usrp_snippet.cpp
@@ -0,0 +1,17 @@
+#include "config.hpp"
+#include "usrp.hpp"
+#include "usrp_interface.hpp"
+int main() {
+    std::string usrpIp = "192.168.189.131";
+    Usrp usrp(usrpIp);
+    RfConfig rfConfig;
+    rfConfig.txGain = 40;
+    rfConfig.rxGain = 30;
+    rfConfig.txCarrierFrequency = 2e9;
+    rfConfig.rxCarrierFrequency = 2e9;
+    rfConfig.txAnalogFilterBw = 400e6;
+    rfConfig.rxAnalogFilterBw = 400e6;
+    rfConfig.txSamplingRate = 200e6;
+    rfConfig.rxSamplingRate = 200e6;
+    usrp.setRfConfig(rfConfig);
+}

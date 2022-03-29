160e828375931327b7d0a1146966ed378d26b0fb
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-03-29 09:09:00 +0000

    choose proper ip address for snippet

--- snippets/usrp_snippet.cpp
+++ snippets/usrp_snippet.cpp
@@ -2,7 +2,7 @@
 #include "usrp.hpp"
 #include "usrp_interface.hpp"
 int main() {
-    std::string usrpIp = "192.168.189.131";
+    std::string usrpIp = "localhost";
     Usrp usrp(usrpIp);
     RfConfig rfConfig;
     rfConfig.txGain = 40;

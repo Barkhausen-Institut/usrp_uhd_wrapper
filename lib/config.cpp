945d2f1cd7dfed886dc7e9fe38887cb3640b779f
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-03-31 12:46:00 +0000

    fix std::ceil

--- lib/config.cpp
+++ lib/config.cpp
@@ -2,7 +2,9 @@
 #include "config.hpp"
 namespace bi {
 size_t calcNoPackages(const size_t noSamples, const size_t spb) {
-    return std::ceil(static_cast<float>(noSamples) / static_cast<float>(spb));
+    // taken from
+    // https://stackoverflow.com/questions/2745074/fast-ceiling-of-an-integer-division-in-c-c
+    return (noSamples + spb - 1) / spb;
 }
 size_t calcNoSamplesLastBuffer(const size_t noSamples, const size_t spb) {
     size_t noSamplesLastBuffer = noSamples % SAMPLES_PER_BUFFER;

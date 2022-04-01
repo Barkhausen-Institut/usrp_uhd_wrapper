26da02026794f47cb7169dd884caea5f534e7278
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-04-01 12:29:16 +0000

    define rf config

--- lib/pybinding/usrp.cpp
+++ lib/pybinding/usrp.cpp
@@ -5,10 +5,22 @@
 
 #include "usrp_interface.hpp"
 
+namespace py = pybind11;
 PYBIND11_MODULE(pymod, m) {
     // factory function
     m.def("createUsrp", &bi::createUsrp, "Creates a USRP with dedicated IP.");
 
     // wrap object
-    pybind11::class_<bi::UsrpInterface>(m, "Usrp");
+    py::class_<bi::RfConfig>(m, "RfConfig")
+        .def(py::init())
+        .def_readwrite("txSamplingRate", &bi::RfConfig::txSamplingRate)
+        .def_readwrite("rxSamplingRate", &bi::RfConfig::rxSamplingRate)
+        .def_readwrite("txAnalogFilterBw", &bi::RfConfig::txAnalogFilterBw)
+        .def_readwrite("rxAnalogFilterBw", &bi::RfConfig::rxAnalogFilterBw)
+        .def_readwrite("txGain", &bi::RfConfig::txGain)
+        .def_readwrite("rxGain", &bi::RfConfig::rxGain)
+        .def_readwrite("txCarrierFrequency", &bi::RfConfig::txCarrierFrequency)
+        .def_readwrite("rxCarrierFrequency", &bi::RfConfig::rxCarrierFrequency);
+    py::class_<bi::UsrpInterface>(m, "Usrp").def(
+        "setRfConfig", &bi::UsrpInterface::setRfConfig);
 }

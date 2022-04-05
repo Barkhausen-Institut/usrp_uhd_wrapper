6998d705815e2e4df8bd683f711adeca3a72ce01
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-04-05 15:57:42 +0200

    use more generic form of zadoff chu

--- hardware_tests/zadoff_chu_localhost.py
+++ hardware_tests/zadoff_chu_localhost.py
@@ -2,36 +2,22 @@ import sys
 sys.path.extend(["release_build/lib/", "debug_build/lib/", "build/lib/"])
 import usrp_pybinding
 import numpy as np
+from utils import (createZadoffChuChirp, getFirstSampleOfSignal)
 
-def createZadoffChuChirp(N: int) -> np.array:
-    M = N+1
-    if (N % 2 == 0):
-        x = np.array([np.exp(1j*np.pi*M*k**2/N) for k in range(N)])
-    else:
-        x = np.array([np.exp(1j*np.pi*M*k*(k+1)/N) for k in range(N)])
-    return x
-
-def getSignalLength(correlation: np.array) -> int:
-    ind1 = abs(np.argsort(correlation)[-1])
-    ind2 = abs(np.argsort(correlation)[-2])
-    
-    signalEnd = ind1 if ind1 > ind2 else ind1
-    signalStart = ind1 if ind1 < ind2 else ind2
-    return signalEnd - signalStart
 
 NO_TX_SAMPLES = int(2e3)
 NO_RX_SAMPLES = int(60e3)
 txSignal = createZadoffChuChirp(NO_TX_SAMPLES)
 
 rfConfig = usrp_pybinding.RfConfig()
-rfConfig.txGain = [50];
-rfConfig.rxGain = [30];
-rfConfig.txCarrierFrequency = [2e9];
-rfConfig.rxCarrierFrequency = [2e9];
-rfConfig.txAnalogFilterBw = 400e6;
-rfConfig.rxAnalogFilterBw = 400e6;
-rfConfig.txSamplingRate = 50e6;
-rfConfig.rxSamplingRate = 50e6;
+rfConfig.txGain = [50]
+rfConfig.rxGain = [30]
+rfConfig.txCarrierFrequency = [2e9]
+rfConfig.rxCarrierFrequency = [2e9]
+rfConfig.txAnalogFilterBw = 400e6
+rfConfig.rxAnalogFilterBw = 400e6
+rfConfig.txSamplingRate = 50e6
+rfConfig.rxSamplingRate = 50e6
 
 rxStreamingConfig = usrp_pybinding.RxStreamingConfig()
 rxStreamingConfig.noSamples = NO_RX_SAMPLES
@@ -52,6 +38,6 @@ usrp.reset()
 
 # post-process
 xcorr = np.abs(np.correlate(samples[0], txSignal))
-signalStartSample = getSignalLength(xcorr)
+signalStartSample = getFirstSampleOfSignal(xcorr)
 print(f"The siganl starts at sample {signalStartSample}")
 breakpoint()

1d5a7fd1f3c6b0ff1a882accc7ef52304ff25c62
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-04-05 14:23:30 +0000

    remove plotting

--- hardware_tests/utils.py
+++ hardware_tests/utils.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 def createZadoffChuChirp(N: int) -> np.array:
     M = 1
@@ -11,18 +10,3 @@ def createZadoffChuChirp(N: int) -> np.array:
 def getFirstSampleOfSignal(frame: np.array, signal: np.array) -> int:
     correlation = np.abs(np.correlate(frame, signal))
     return np.argsort(correlation)[-1]
-
-
-if __name__ == "__main__":
-    NO_SAMPLES_FRAME = int(2e3)
-    FIRST_SAMPLE_CHIRP = 62
-    LENGTH_CHIRP = NO_SAMPLES_FRAME - FIRST_SAMPLE_CHIRP
-    chirp = createZadoffChuChirp(LENGTH_CHIRP)
-    frame = np.zeros(NO_SAMPLES_FRAME, dtype=np.complex64)
-    frame[FIRST_SAMPLE_CHIRP:] = chirp
-
-    print(getFirstSampleOfSignal(frame, chirp))
-    plt.figure()
-    plt.plot(np.arange(NO_SAMPLES_FRAME), np.real(frame))
-    plt.plot(np.arange(NO_SAMPLES_FRAME), np.imag(frame))
-    plt.show()

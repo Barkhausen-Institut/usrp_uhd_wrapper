6ae76199cbf131511a8b94fcdb34339df3ee3846
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-04-11 15:37:40 +0200

    add deserialization

--- rpcserver/rpc_server.py
+++ rpcserver/rpc_server.py
@@ -8,3 +8,11 @@ def serializeComplexArray(data: np.ndarray) -> Tuple[List, List]:
     if (len(data.shape) == 2):
         raise ValueError("Array must be one dimensional!")
     return (np.real(data).tolist(), np.imag(data).tolist())
+
+
+def deserializeComplexArray(data: Tuple[List, List]) -> np.ndarray:
+    if len(data[0]) != len(data[1]):
+        raise ValueError("""Number of imaginary samples
+                            mismatches number of real samples.""")
+    arr = np.array(data[0]) + 1j * np.array(data[1])
+    return arr
--- tests/python/test_rpc_server.py
+++ tests/python/test_rpc_server.py
@@ -2,11 +2,12 @@ from multiprocessing.sharedctypes import Value
 import unittest
 
 import numpy as np
+import numpy.testing as npt
 
-from rpcserver.rpc_server import serializeComplexArray
+from rpcserver.rpc_server import serializeComplexArray, deserializeComplexArray
 
 
-class TestSerializationCustomObjects(unittest.TestCase):
+class TestSerializationComplexArr(unittest.TestCase):
     def test_oneDimensionalArray_complexValues(self) -> None:
         oneDimensionalArr = np.ones(3, dtype=np.complex64)
         real, imag = serializeComplexArray(oneDimensionalArr)
@@ -24,3 +25,19 @@ class TestSerializationCustomObjects(unittest.TestCase):
     def test_ndArrayShouldNotBeSupported(self) -> None:
         arr = np.ones((2, 3))
         self.assertRaises(ValueError, lambda: serializeComplexArray(arr))
+
+
+class TestDeserializationComplexArr(unittest.TestCase):
+    def test_properDeserialization(self) -> None:
+        imagList = [1, 2, 3]
+        realList = [4, 5, 6]
+
+        expectedArr = np.array(realList) + 1j * np.array(imagList)
+        deserializedArr = deserializeComplexArray((realList, imagList))
+        npt.assert_array_almost_equal(deserializedArr, expectedArr)
+
+    def test_noImagSamples_mismatch_noRealSamples(self) -> None:
+        imagList = [0]
+        realList = [1, 2]
+
+        self.assertRaises(ValueError, lambda: deserializeComplexArray((realList, imagList)))

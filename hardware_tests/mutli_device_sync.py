7d7216eec4503f7c418d1bbc20d4724e7e76d25c
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-04-11 07:33:25 +0000

    add first draft of mutli device sync

---
+++ hardware_tests/mutli_device_sync.py
@@ -0,0 +1,42 @@
+from json import dumps
+import sys
+from hardware_tests.send_signal_localhost import NO_RX_SAMPLES, NO_TX_SAMPLES
+sys.path.extend(["release_build/lib/", "debug_build/lib/", "build/lib/"])
+import usrp_pybinding
+import numpy as np
+from utils import RandomSignal, findFirstSampleInFrameOfSignal, dumpSamples
+
+NO_TX_SAMPLES = 10e3
+NO_RX_SAMPLES = 60e3
+
+txSignal = RandomSignal()
+txSignal.create(NO_TX_SAMPLES, 1)
+
+rfConfig = usrp_pybinding.RfConfig()    
+rfConfig.txGain = [50]
+rfConfig.rxGain = [30]
+rfConfig.txCarrierFrequency = [2e9]
+rfConfig.rxCarrierFrequency = [2e9]
+rfConfig.txAnalogFilterBw = 400e6
+rfConfig.rxAnalogFilterBw = 400e6
+rfConfig.txSamplingRate = 10e6
+rfConfig.rxSamplingRate = 10e6
+
+rxStreamingConfig = usrp_pybinding.RxStreamingConfig()
+rxStreamingConfig.noSamples = NO_RX_SAMPLES
+rxStreamingConfig.receiveTimeOffset = 2.0
+
+txStreamingConfig = usrp_pybinding.TxStreamingConfig()
+txStreamingConfig.samples = [txSignal.samples]
+txStreamingConfig.sendTimeOffset = 2.0
+
+ip = "localhost"
+usrp = usrp_pybinding.createUsrp(ip)
+usrp.setRfConfig(rfConfig)
+usrp.setRxConfig(rxStreamingConfig)
+_ = input("Press enter to synchronize devices and to continue")
+usrp.setTimeToZeroNextPps()
+samples = usrp.execute(0.0)
+print(f"Tx signal starts at sample {findFirstSampleInFrameOfSignal(samples[0], txSignal.samples)} in the received frame.")
+dumpSamples("rxSamples.csv", samples[0])
+usrp.reset()

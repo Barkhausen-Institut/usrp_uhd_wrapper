14f99e2fee57afd318e9a62bec888eb1bd38792b
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-04-11 15:22:08 +0200

    test for non-complex arrays

--- tests/python/test_rpc_server.py
+++ tests/python/test_rpc_server.py
@@ -12,3 +12,10 @@ class TestSerializationCustomObjects(unittest.TestCase):
 
         self.assertListEqual(imag, np.imag(oneDimensionalArr).tolist())
         self.assertListEqual(real, np.real(oneDimensionalArr).tolist())
+
+    def test_nonComplexArrayYieldsZeroImaginary(self) -> None:
+        arr = np.ones(3)
+        real, imag = serializeComplexArray(arr)
+
+        self.assertListEqual(imag, np.imag(arr).tolist())
+        self.assertListEqual(real, np.real(arr).tolist())

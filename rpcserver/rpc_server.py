b4adebbf24118d6987fca64842412b80d12d62bd
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-04-13 17:18:12 +0200

    add minimal version of usrp server

--- rpcserver/rpc_server.py
+++ rpcserver/rpc_server.py
@@ -1,18 +1,38 @@
+from typing import Tuple, List
+import sys
+import os
+
+sys.path.extend([os.path.join("build", "lib"), os.path.join("release_build", "lib")])
 import numpy as np
 
-from typing import Tuple, List
+from usrp_pybinding import Usrp, TxStreamingConfig, RxStreamingConfig, RfConfig
 
 
 def serializeComplexArray(data: np.ndarray) -> Tuple[List, List]:
     data = np.squeeze(data)
-    if (len(data.shape) == 2):
+    if len(data.shape) == 2:
         raise ValueError("Array must be one dimensional!")
     return (np.real(data).tolist(), np.imag(data).tolist())
 
 
 def deserializeComplexArray(data: Tuple[List, List]) -> np.ndarray:
     if len(data[0]) != len(data[1]):
-        raise ValueError("""Number of imaginary samples
-                            mismatches number of real samples.""")
+        raise ValueError(
+            """Number of imaginary samples
+                            mismatches number of real samples."""
+        )
     arr = np.array(data[0]) + 1j * np.array(data[1])
     return arr
+
+
+class UsrpServer:
+    def __init__(self, usrp: Usrp) -> None:
+        self.__usrp = usrp
+
+    def configureTx(self, timeOffset: float, buffers: List[Tuple[List, List]]) -> None:
+        self.__usrp.setTxConfig(
+            TxStreamingConfig(
+                samples=[deserializeComplexArray(buffer) for buffer in buffers],
+                sendTimeOffset=timeOffset,
+            )
+        )

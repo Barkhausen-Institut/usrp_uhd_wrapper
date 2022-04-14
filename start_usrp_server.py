45843daa949598c73f820c89193f359ff0bb62f1
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-04-14 14:40:22 +0200

    rename

--- start_usrp_server.py
+++ start_usrp_server.py
@@ -9,12 +9,12 @@ from usrp_pybinding import Usrp, createUsrp
 
 # create environment
 PORT = 5555
-IP_SERVER = "localhost"
-usrp = createUsrp(IP_SERVER)
+IP_USRP = "localhost"
+usrp = createUsrp(IP_USRP)
 
 # start server
 rpcServer = zerorpc.Server(UsrpServer(usrp))
 rpcServer.bind(f"tcp://*:{PORT}")
 rpcServer.run()
 
-print(f"Created USRP server on IP {IP_SERVER}, listening on port {PORT}")
+print(f"Created USRP server on IP {IP_USRP}, listening on port {PORT}")

eca69bc8ec118452dfeac1ac58e18afd0d6558bc
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-04-20 13:26:13 +0200

    add utils folder

--- rpc_server/rpc_server.py
+++ rpc_server/rpc_server.py
@@ -6,26 +6,11 @@ sys.path.extend([os.path.join("build", "lib"), os.path.join("release_build", "li
 import numpy as np
 
 from usrp_pybinding import Usrp, TxStreamingConfig, RxStreamingConfig, RfConfig
-
-
-SerializedComplexArray = Tuple[List, List]
-
-
-def serializeComplexArray(data: np.ndarray) -> SerializedComplexArray:
-    data = np.squeeze(data)
-    if len(data.shape) == 2:
-        raise ValueError("Array must be one dimensional!")
-    return (np.real(data).tolist(), np.imag(data).tolist())
-
-
-def deserializeComplexArray(data: SerializedComplexArray) -> np.ndarray:
-    if len(data[0]) != len(data[1]):
-        raise ValueError(
-            """Number of imaginary samples
-                            mismatches number of real samples."""
-        )
-    arr = np.array(data[0]) + 1j * np.array(data[1])
-    return arr
+from utils.serialization import (
+    serializeComplexArray,
+    deserializeComplexArray,
+    SerializedComplexArray,
+)
 
 
 class UsrpServer:
---
+++ utils/serialization.py
@@ -0,0 +1,47 @@
+from typing import List, Tuple
+from dataclasses import dataclass, field
+
+import numpy as np
+
+SerializedComplexArray = Tuple[List, List]
+
+
+def serializeComplexArray(data: np.ndarray) -> SerializedComplexArray:
+    data = np.squeeze(data)
+    if len(data.shape) == 2:
+        raise ValueError("Array must be one dimensional!")
+    return (np.real(data).tolist(), np.imag(data).tolist())
+
+
+def deserializeComplexArray(data: SerializedComplexArray) -> np.ndarray:
+    if len(data[0]) != len(data[1]):
+        raise ValueError(
+            """Number of imaginary samples
+                            mismatches number of real samples."""
+        )
+    arr = np.array(data[0]) + 1j * np.array(data[1])
+    return arr
+
+
+@dataclass
+class RfConfig:
+    txAnalogFilterBw: float = 0.0
+    rxAnalogFilterBw: float = 0.0
+    txSamplingRate: float = 0.0
+    rxSamplingRate: float = 0.0
+    txGain: List[float] = field(default_factory=list)
+    rxGain: List[float] = field(default_factory=list)
+    txCarrierFrequency: List[float] = field(default_factory=list)
+    rxCarrierFrequency: List[float] = field(default_factory=list)
+
+
+@dataclass
+class TxStreamingConfig:
+    sendTimeOffset: float = 0.0
+    samples: List[np.ndarray] = field(default_factory=list)
+
+
+@dataclass
+class RxStreamingConfig:
+    receiveTimeOffset: float = 0.0
+    noSamples: int = 0

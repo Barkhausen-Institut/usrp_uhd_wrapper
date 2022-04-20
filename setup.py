0f77210375f07f985990b6f17ada5288245b16ee
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-04-20 13:40:22 +0200

    add install_requires and remove rpc_server but add usrp_client

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup
 setup(
     name="usrp_uhd_api",
     version="0.0.0",
-    packages=["rpc_server", "utils"],
+    packages=["utils", "usrp_client"],
     python_requires=">=3.9",
+    install_requires=[
+        "zerorpc~=0.6.3",
+        "pyzmq~=22.3.0",
+        "numpy~=1.21.6",
+        "greenlet==0.4.16",
+    ],
 )

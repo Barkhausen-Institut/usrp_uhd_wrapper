73cf326c927303951863475b4fa29764f106d6f7
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-05-02 15:03:54 +0200

    add plotting

--- examples/usrp_p2p_transmission.py
+++ examples/usrp_p2p_transmission.py
@@ -6,6 +6,7 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 from usrp_client.system import System
 from uhd_wrapper.utils.config import RfConfig, TxStreamingConfig, RxStreamingConfig
 
@@ -58,6 +59,7 @@ args = parser.parse_args()
 txSignal = createRandom(int(20e3), zeropad=2000)
 
 # create configurations
+NO_RX_SAMPLES = 60e3
 rfConfig = RfConfig()
 rfConfig.rxAnalogFilterBw = 400e6
 rfConfig.txAnalogFilterBw = 400e6
@@ -69,10 +71,14 @@ rfConfig.rxCarrierFrequency = [args.carrier_frequency]
 rfConfig.txCarrierFrequency = [args.carrier_frequency]
 
 txStreamingConfig1 = TxStreamingConfig(sendTimeOffset=0.0, samples=[txSignal])
-rxStreamingConfig1 = RxStreamingConfig(receiveTimeOffset=0.1, noSamples=int(60e3))
+rxStreamingConfig1 = RxStreamingConfig(
+    receiveTimeOffset=0.1, noSamples=int(NO_RX_SAMPLES)
+)
 
 txStreamingConfig2 = TxStreamingConfig(sendTimeOffset=0.1, samples=[txSignal])
-rxStreamingConfig2 = RxStreamingConfig(receiveTimeOffset=0.0, noSamples=int(60e3))
+rxStreamingConfig2 = RxStreamingConfig(
+    receiveTimeOffset=0.0, noSamples=int(NO_RX_SAMPLES)
+)
 
 # ceate system
 system = System()
@@ -95,5 +101,39 @@ txSignalStartUsrp1, correlationUsrp1 = findFirstSampleInFrameOfSignal(
     samples["usrp1"][0], txSignal
 )
 
-print(f"Sent signal from usrp2 starts at sample {txSignalStartUsrp1} in usrp1")
-print(f"Sent signal from usrp1 starts at sample {txSignalStartUsrp2} in usrp2")
+print(f"Sent random signal from usrp2 starts at sample {txSignalStartUsrp1} in usrp1")
+print(f"Sent random signal from usrp1 starts at sample {txSignalStartUsrp2} in usrp2")
+
+if args.plot:
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    rxSpectrumUsrp1 = np.fft.fftshift(np.fft.fft(samples["usrp1"][0]))
+    rxFreqSpectrumUsrp1 = np.fft.fftshift(np.fft.fftfreq(samples["usrp1"][0].size))
+
+    rxSpectrumUsrp2 = np.fft.fftshift(np.fft.fft(samples["usrp2"][0]))
+    rxFreqSpectrumUsrp2 = np.fft.fftshift(np.fft.fftfreq(samples["usrp2"][0].size))
+    plt.subplot(221)
+    plt.plot(np.arange(NO_RX_SAMPLES), samples["usrp1"][0])
+    plt.xlabel("Samples [#]")
+    plt.ylabel("Value")
+    plt.title("Usrp1, received samples, time")
+
+    plt.subplot(222)
+    plt.plot(np.arange(NO_RX_SAMPLES), samples["usrp2"][0])
+    plt.xlabel("Samples [#]")
+    plt.ylabel("Value")
+    plt.title("Usrp2, received samples, time")
+
+    plt.subplot(223)
+    plt.semilogy(rxFreqSpectrumUsrp1 / 1e6, np.abs(rxSpectrumUsrp1))
+    plt.xlabel("Frequency [Mhz]")
+    plt.ylabel("Power [log]")
+    plt.title("Spectrum USRP1")
+
+    plt.subplot(224)
+    plt.semilogy(rxFreqSpectrumUsrp2 / 1e6, np.abs(rxSpectrumUsrp2))
+    plt.xlabel("Frequency [Mhz]")
+    plt.ylabel("Power [log]")
+    plt.title("Spectrum USRP2")
+    plt.show()

e94e3f7add8833646ab36c5f96d921bd2d41195c
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-05-02 14:52:04 +0200

    add pairwise transmission

--- examples/usrp_p2p_transmission.py
+++ examples/usrp_p2p_transmission.py
@@ -1,6 +1,7 @@
 from typing import Tuple
 import logging
 import time
+import argparse
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -39,64 +40,60 @@ def findFirstSampleInFrameOfSignal(
     return np.argsort(correlation)[-1], correlation
 
 
-logging.basicConfig(level=logging.INFO)
-c = RfConfig()
-c.rxAnalogFilterBw = 400e6
-c.txAnalogFilterBw = 400e6
-c.rxSamplingRate = 245.76e6 / 2
-c.txSamplingRate = 245.76e6 / 2
-c.rxGain = [35]
-c.txGain = [35]
-c.rxCarrierFrequency = [2e9]
-c.txCarrierFrequency = [2e9]
+parser = argparse.ArgumentParser()
+parser.add_argument("--usrp1-ip", type=str, help="IP of first USRP")
+parser.add_argument("--usrp2-ip", type=str, help="IP of second USRP")
+parser.add_argument(
+    "--carrier-frequency", type=float, help="Carrier frequency of sent signal"
+)
+parser.add_argument(
+    "--plot",
+    type=bool,
+    default=False,
+    help="Plot received singals in time and frequency",
+)
+args = parser.parse_args()
 
+# create signal to be se nt
 txSignal = createRandom(int(20e3), zeropad=2000)
+
+# create configurations
+rfConfig = RfConfig()
+rfConfig.rxAnalogFilterBw = 400e6
+rfConfig.txAnalogFilterBw = 400e6
+rfConfig.rxSamplingRate = 245.76e6 / 2
+rfConfig.txSamplingRate = 245.76e6 / 2
+rfConfig.rxGain = [35]
+rfConfig.txGain = [35]
+rfConfig.rxCarrierFrequency = [args.carrier_frequency]
+rfConfig.txCarrierFrequency = [args.carrier_frequency]
+
 txStreamingConfig1 = TxStreamingConfig(sendTimeOffset=0.0, samples=[txSignal])
 rxStreamingConfig1 = RxStreamingConfig(receiveTimeOffset=0.1, noSamples=int(60e3))
 
 txStreamingConfig2 = TxStreamingConfig(sendTimeOffset=0.1, samples=[txSignal])
 rxStreamingConfig2 = RxStreamingConfig(receiveTimeOffset=0.0, noSamples=int(60e3))
 
+# ceate system
 system = System()
-system.addUsrp(rfConfig=c, ip="192.168.189.133", usrpName="usrp1")
-system.addUsrp(rfConfig=c, ip="192.168.189.131", usrpName="usrp2")
+system.addUsrp(rfConfig=rfConfig, ip=args.usrp1_ip, usrpName="usrp1")
+system.addUsrp(rfConfig=rfConfig, ip=args.usrp2_ip, usrpName="usrp2")
 
-for iteration in range(3):
-    start = time.time()
-    system.configureTx(usrpName="usrp1", txStreamingConfig=txStreamingConfig1)
-    print(f"Config 1 Took {time.time() - start} seconds")
-    system.configureRx(usrpName="usrp1", rxStreamingConfig=rxStreamingConfig1)
-    print(f"Config 1RX Took {time.time() - start} seconds")
+system.configureTx(usrpName="usrp1", txStreamingConfig=txStreamingConfig1)
+system.configureRx(usrpName="usrp1", rxStreamingConfig=rxStreamingConfig1)
 
-    system.configureTx(usrpName="usrp2", txStreamingConfig=txStreamingConfig2)
-    system.configureRx(usrpName="usrp2", rxStreamingConfig=rxStreamingConfig2)
-    print(f"Config 2 Took {time.time() - start} seconds")
+system.configureTx(usrpName="usrp2", txStreamingConfig=txStreamingConfig2)
+system.configureRx(usrpName="usrp2", rxStreamingConfig=rxStreamingConfig2)
 
-    system.execute()
-    print(f"EXecute Took {time.time() - start} seconds")
+system.execute()
+samples = system.collect()
+txSignalStartUsrp2, correlationUsrp2 = findFirstSampleInFrameOfSignal(
+    samples["usrp2"][0], txSignal
+)
 
-    samples = system.collect()
-    print(f"Took {time.time() - start} seconds")
+txSignalStartUsrp1, correlationUsrp1 = findFirstSampleInFrameOfSignal(
+    samples["usrp1"][0], txSignal
+)
 
-    txSignalStartUsrp2, correlationUsrp2 = findFirstSampleInFrameOfSignal(
-        samples["usrp2"][0], txSignal
-    )
-
-    txSignalStartUsrp1, correlationUsrp1 = findFirstSampleInFrameOfSignal(
-        samples["usrp1"][0], txSignal
-    )
-
-    print(f"Sent signal from usrp2 starts at sample {txSignalStartUsrp1} in usrp1")
-    print(f"Sent signal from usrp1 starts at sample {txSignalStartUsrp2} in usrp2")
-
-    plt.subplot(221)
-    plt.plot(np.arange(60e3), np.abs(samples["usrp1"][0]))
-    plt.title("Received samples usrp1")
-    plt.subplot(222)
-    plt.plot(np.arange(60e3), np.abs(samples["usrp2"][0]))
-    plt.title("Received samples usrp2")
-    plt.subplot(223)
-    plt.plot(np.arange(correlationUsrp1.size), np.abs(correlationUsrp1))
-    plt.subplot(224)
-    plt.plot(np.arange(correlationUsrp2.size), np.abs(correlationUsrp2))
-    plt.show()
+print(f"Sent signal from usrp2 starts at sample {txSignalStartUsrp1} in usrp1")
+print(f"Sent signal from usrp1 starts at sample {txSignalStartUsrp2} in usrp2")

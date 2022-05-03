1181666ad355faef59cbfd9f7cb52600f39cf3ef
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-05-03 15:26:39 +0200

    add docs to rpc client

--- usrp_client/rpc_client.py
+++ usrp_client/rpc_client.py
@@ -12,25 +12,59 @@ from uhd_wrapper.utils.config import RxStreamingConfig, TxStreamingConfig, RfCon
 
 
 class UsrpClient:
+    """This class is the RPC client to the RPC server running on the USRP
+
+    Under the hood, we communicate with the UsrpServer class which wraps zerorpc. ZeroRPC is
+    implements remote procedure call client-server architecture using zeromq as a communication
+    protocol. `UsrpClient` forwards user calls to the RPC server and serializes them if
+    required.
+    """
+
     def __init__(self, rpcClient: zerorpc.Client) -> None:
+        """Initializes the UsrpClient.
+
+        Args:
+            rpcClient (zerorpc.Client): zerorpc.Client that is already connected to RPC server.
+        """
         self.__rpcClient = rpcClient
 
     def configureRx(self, rxConfig: RxStreamingConfig) -> None:
+        """Call `configureRx` on server and serialize `rxConfig`.
+
+        Args:
+            rxConfig (RxStreamingConfig): Streaming config.
+        """
         self.__rpcClient.configureRx(rxConfig.receiveTimeOffset, rxConfig.noSamples)
 
     def configureTx(self, txConfig: TxStreamingConfig) -> None:
+        """Call `configureTx` on server and serialize `txConfig`."""
         self.__rpcClient.configureTx(
             txConfig.sendTimeOffset,
             [serializeComplexArray(antSignal) for antSignal in txConfig.samples],
         )
 
     def execute(self, baseTime: float) -> None:
+        """Execute the current configuration at the receiver side.
+
+        Set the baseTime on the receiver to the desired value.
+
+        Args:
+            baseTime (float): Base time to intialize the FPGA time to.
+        """
         self.__rpcClient.execute(baseTime)
 
     def collect(self) -> List[np.ndarray]:
+        """Collect samples from RPC server and deserialize them.
+
+        Returns:
+            List[np.ndarray]:
+                Each list item corresponds to the samples of one antenna.
+                Samples are of type np.complex64.
+        """
         return [deserializeComplexArray(frame) for frame in self.__rpcClient.collect()]
 
     def configureRfConfig(self, rfConfig: RfConfig) -> None:
+        """Serialize `rfConfig` and request configuration on RPC server."""
         self.__rpcClient.configureRfConfig(
             rfConfig.txGain,
             rfConfig.rxGain,
@@ -43,13 +77,17 @@ class UsrpClient:
         )
 
     def setTimeToZeroNextPps(self) -> None:
+        """Sets the time to zero on the next PPS edge."""
         self.__rpcClient.setTimeToZeroNextPps()
 
     def getCurrentFpgaTime(self) -> int:
+        """Queries current FPGA time from RPC server."""
         return self.__rpcClient.getCurrentFpgaTime()
 
     def getCurrentSystemTime(self) -> int:
+        """Queries current system time from RPC server."""
         return self.__rpcClient.getCurrentSystemTime()
 
     def getRfConfig(self) -> RfConfig:
+        """Queries RfConfig from RPC server and deserializes it."""
         return deserializeRfConfig(self.__rpcClient.getRfConfig())

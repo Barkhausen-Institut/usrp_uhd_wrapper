cd69c1c2f66ccd77f961b8b32bb0796f90e256c2
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-05-04 15:59:22 +0200

    put fillDummyRfConfig in config

--- uhd_wrapper/utils/config.py
+++ uhd_wrapper/utils/config.py
@@ -1,8 +1,10 @@
-from typing import List
+from typing import List, Union
 from dataclasses import dataclass, field
 from dataclasses_json import dataclass_json
 
 import numpy as np
+
+from uhd_wrapper.usrp_pybinding import RfConfig as RfConfigBinding
 
 
 @dataclass_json
@@ -28,3 +30,18 @@ class TxStreamingConfig:
 class RxStreamingConfig:
     receiveTimeOffset: float = 0.0
     noSamples: int = 0
+
+
+def fillDummyRfConfig(
+    conf: Union[RfConfig, RfConfigBinding]
+) -> Union[RfConfig, RfConfigBinding]:
+    conf.txCarrierFrequency = [2e9]
+    conf.txGain = [30]
+    conf.txAnalogFilterBw = 200e6
+    conf.txSamplingRate = 20e6
+
+    conf.rxCarrierFrequency = [2.5e9]
+    conf.rxGain = [40]
+    conf.rxAnalogFilterBw = 100e6
+    conf.rxSamplingRate = 30e6
+    return conf

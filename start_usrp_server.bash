47144fb96ac07a1ffdce8d041eec6510453d722f
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-05-04 07:24:55 +0000

    export HOME and XDG_config_home variables to make bash script runnable by service

--- start_usrp_server.bash
+++ start_usrp_server.bash
@@ -1,3 +1,7 @@
+export XDG_CONFIG_HOME=/home/root/
+export HOME=/home/root/
 cd /home/root/usrp_uhd_api/
 . env/bin/activate
-python start_usrp_server.py
+echo "$(date +%d.%m.%Y:%H:%M:%S): Started USRP server." >> usrp_server_log.txt
+python start_usrp_server.py &>>usrp_sever_log.txt
+echo "$(date +%d.%m.%Y:%H:%M:%S): Stopped USRP server." >> usrp_server_log.txt

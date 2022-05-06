0e1eb4c66a58d9aae2c0b714a7d282d9adf29649
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-05-06 11:44:53 +0200

    mypy

--- usrp_client/tests/test_rpc_client.py
+++ usrp_client/tests/test_rpc_client.py
@@ -77,18 +77,20 @@ class TestUsrpClient(unittest.TestCase):
         self.assertAlmostEqual(self.usrpClient.getCurrentSystemTime(), TIME)
 
     def test_getMasterClockRate_functionGetsCalled(self) -> None:
-        self.usrpClient.getSupportedDecimationRatios = lambda: np.array([1.0])
+        self.usrpClient.getSupportedDecimationRatios = lambda: np.array([1.0])  # type: ignore
         _ = self.usrpClient.getMasterClockRate()
         self.mockRpcClient.getMasterClockRate.assert_called_once()
 
     def test_supportedSamplingRates_queriesMasterClockRate(self) -> None:
-        self.usrpClient.getSupportedDecimationRatios = lambda: np.array([1.0])
+        self.usrpClient.getSupportedDecimationRatios = lambda: np.array([1.0])  # type: ignore
         _ = self.usrpClient.getSupportedSamplingRates()
         self.mockRpcClient.getMasterClockRate.assert_called_once()
 
     def test_supportedSamplingRates(self) -> None:
         supportedDecimationRatios = np.array([1, 2])
-        self.usrpClient.getSupportedDecimationRatios = lambda: supportedDecimationRatios
+        self.usrpClient.getSupportedDecimationRatios = (  # type: ignore
+            lambda: supportedDecimationRatios
+        )
         npt.assert_array_almost_equal(
             self.masterClockRate / supportedDecimationRatios,
             self.usrpClient.getSupportedSamplingRates(),

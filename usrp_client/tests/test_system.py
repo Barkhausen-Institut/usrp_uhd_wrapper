604e1eb62ca44ef2b43b3a361d1f19fc45dddaa6
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-05-09 15:03:49 +0200

    introduce hardwaresetup module

--- usrp_client/tests/test_system.py
+++ usrp_client/tests/test_system.py
@@ -1,8 +1,7 @@
 import unittest
 from unittest.mock import Mock, patch
-from typing import List, Tuple, Any
+from typing import List
 import pytest
-from abc import ABC, abstractmethod
 
 import numpy as np
 import numpy.testing as npt
@@ -15,6 +14,8 @@ from uhd_wrapper.utils.config import (
     TxStreamingConfig,
     RxStreamingConfig,
 )
+
+from usrp_client.tests.hardware_setups import LocalTransmissionHWSetup, P2PHardwareSetup
 
 
 class TestSystemInitialization(unittest.TestCase):
@@ -236,73 +237,6 @@ class TestTransceivingMultiDevice(unittest.TestCase, SystemMockFactory):
         ]
 
         self.assertRaises(ValueError, lambda: self.system.collect())
-
-
-class HardwareSetup(ABC):
-    @abstractmethod
-    def createSystem(self) -> System:
-        pass
-
-    @abstractmethod
-    def createStreamingConfigs(self, txSignal: np.ndarray) -> Any:
-        pass
-
-
-class P2PHardwareSetup(HardwareSetup):
-    def createSystem(self) -> System:
-        rfConfig = RfConfig()
-        rfConfig.rxAnalogFilterBw = 400e6
-        rfConfig.txAnalogFilterBw = 400e6
-        rfConfig.rxSamplingRate = 245.76e6
-        rfConfig.txSamplingRate = 245.76e6
-        rfConfig.rxGain = [35]
-        rfConfig.txGain = [35]
-        rfConfig.rxCarrierFrequency = [2e9]
-        rfConfig.txCarrierFrequency = [2e9]
-
-        system = System()
-        system.addUsrp(rfConfig=rfConfig, ip="192.168.189.132", usrpName="usrp1")
-        system.addUsrp(rfConfig=rfConfig, ip="192.168.189.133", usrpName="usrp2")
-        return system
-
-    def createStreamingConfigs(
-        self, txSignal: np.ndarray
-    ) -> Tuple[TxStreamingConfig, RxStreamingConfig]:
-        txStreamingConfig1 = TxStreamingConfig(
-            sendTimeOffset=0.0, samples=MimoSignal(signals=[txSignal])
-        )
-        rxStreamingConfig2 = RxStreamingConfig(
-            receiveTimeOffset=0.0, noSamples=int(60e3)
-        )
-        return txStreamingConfig1, rxStreamingConfig2
-
-
-class LocalTransmissionHWSetup(HardwareSetup):
-    def createSystem(self) -> System:
-        rfConfig = RfConfig()
-        rfConfig.rxAnalogFilterBw = 400e6
-        rfConfig.txAnalogFilterBw = 400e6
-        rfConfig.rxSamplingRate = 245.76e6
-        rfConfig.txSamplingRate = 245.76e6
-        rfConfig.rxGain = [35]
-        rfConfig.txGain = [35]
-        rfConfig.rxCarrierFrequency = [2e9]
-        rfConfig.txCarrierFrequency = [2e9]
-
-        system = System()
-        system.addUsrp(rfConfig=rfConfig, ip="192.168.189.132", usrpName="usrp1")
-        return system
-
-    def createStreamingConfigs(
-        self, txSignal: np.ndarray
-    ) -> Tuple[TxStreamingConfig, RxStreamingConfig]:
-        txStreamingConfig1 = TxStreamingConfig(
-            sendTimeOffset=0.0, samples=MimoSignal(signals=[txSignal])
-        )
-        rxStreamingConfig1 = RxStreamingConfig(
-            receiveTimeOffset=0.0, noSamples=int(60e3)
-        )
-        return txStreamingConfig1, rxStreamingConfig1
 
 
 @pytest.mark.hardware

4f5df6c7c7fa418ff4c5122606e7473e66d13cb6
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-05-09 11:24:07 +0200

    adapt test

--- usrp_client/tests/test_system.py
+++ usrp_client/tests/test_system.py
@@ -192,7 +192,7 @@ class TestTransceivingMultiDevice(unittest.TestCase, SystemMockFactory):
 
         self.mockUsrps[0].getCurrentFpgaTime.return_value = FPGA_TIME_S_USRP1
         self.mockUsrps[1].getCurrentFpgaTime.return_value = FPGA_TIME_S_USRP2
-        self.assertRaises(ValueError, lambda: self.system.execute())
+        self.assertRaises(RuntimeError, lambda: self.system.execute())
 
     def test_getSamplingRates(self) -> None:
         supportedSamplingRates = np.array([200e6])

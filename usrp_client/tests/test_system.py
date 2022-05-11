55f6ba61fa0cecc0764d5f41a375884115bf7ea1
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-05-11 09:37:11 +0200

    move integration tests into its own file

--- usrp_client/tests/test_system.py
+++ usrp_client/tests/test_system.py
@@ -1,7 +1,6 @@
 import unittest
 from unittest.mock import Mock, patch
-from typing import List, Tuple
-import pytest
+from typing import List
 
 import numpy as np
 import numpy.testing as npt
@@ -13,11 +12,6 @@ from uhd_wrapper.utils.config import (
     RfConfig,
     TxStreamingConfig,
     RxStreamingConfig,
-)
-
-from usrp_client.tests.hardware_setups import (
-    LocalTransmissionHardwareSetup,
-    P2pHardwareSetup,
 )
 
 
@@ -240,115 +234,3 @@ class TestTransceivingMultiDevice(unittest.TestCase, SystemMockFactory):
         ]
 
         self.assertRaises(ValueError, lambda: self.system.collect())
-
-
-@pytest.mark.hardware
-class TestHardwareSystemTests(unittest.TestCase):
-    def setUp(self) -> None:
-        self.noSamples = int(20e3)
-        self.randomSignal = (
-            np.random.sample((self.noSamples,))
-            + 1j * np.random.sample((self.noSamples,))
-        ) - (0.5 + 0.5j)
-
-    def findSignalStartsInFrame(self, frame: np.ndarray, txSignal: np.ndarray) -> int:
-        correlation = np.abs(np.correlate(frame, txSignal))
-        return np.argsort(correlation)[-1]
-
-    def test_p2pTransmission(self) -> None:
-        setup = P2pHardwareSetup(
-            txGain=[35],
-            rxGain=[35],
-            rxSampleRate=245.76e6,
-            txSampleRate=245.76e6,
-            txFc=[2e9],
-            rxFc=[2e9],
-        )
-        system = setup.connectUsrps()
-        txStreamingConfig1 = TxStreamingConfig(
-            sendTimeOffset=0.0, samples=MimoSignal(signals=[self.randomSignal])
-        )
-        rxStreamingConfig2 = RxStreamingConfig(
-            receiveTimeOffset=0.0, noSamples=int(60e3)
-        )
-        system.configureTx(usrpName="usrp1", txStreamingConfig=txStreamingConfig1)
-        system.configureRx(usrpName="usrp2", rxStreamingConfig=rxStreamingConfig2)
-        system.execute()
-        samplesSystems = system.collect()
-        rxSamplesUsrp2 = samplesSystems["usrp2"][0].signals[0]
-
-        self.assertAlmostEqual(
-            first=self.findSignalStartsInFrame(rxSamplesUsrp2, self.randomSigna),
-            second=300,
-            delta=10,
-        )
-
-    def test_localTransmission(self) -> None:
-        setup = LocalTransmissionHardwareSetup(
-            txGain=[35],
-            rxGain=[35],
-            rxSampleRate=245.76e6,
-            txSampleRate=245.76e6,
-            txFc=[2e9],
-            rxFc=[2e9],
-        )
-        system = setup.connectUsrps()
-        txStreamingConfig1 = TxStreamingConfig(
-            sendTimeOffset=0.0, samples=MimoSignal(signals=[self.randomSignal])
-        )
-        rxStreamingConfig1 = RxStreamingConfig(
-            receiveTimeOffset=0.0, noSamples=int(60e3)
-        )
-
-        system.configureTx(usrpName="usrp1", txStreamingConfig=txStreamingConfig1)
-        system.configureRx(usrpName="usrp1", rxStreamingConfig=rxStreamingConfig1)
-
-        system.execute()
-        samplesSystem = system.collect()
-        rxSamplesUsrp1 = samplesSystem["usrp1"][0].signals[0]
-
-        self.assertAlmostEqual(
-            first=self.findSignalStartsInFrame(rxSamplesUsrp1, self.randomSigna),
-            second=300,
-            delta=10,
-        )
-
-    def test_jcas(self) -> None:
-        setup = P2pHardwareSetup(
-            txGain=[35],
-            rxGain=[35],
-            rxSampleRate=245.76e6,
-            txSampleRate=245.76e6,
-            txFc=[2e9],
-            rxFc=[2e9],
-        )
-        system = setup.connectUsrps()
-        txStreamingConfig1 = TxStreamingConfig(
-            sendTimeOffset=0.1, samples=MimoSignal(signals=[self.randomSignal])
-        )
-        rxStreamingConfig1 = RxStreamingConfig(
-            receiveTimeOffset=0.1, noSamples=int(60e3)
-        )
-        rxStreamingConfig2 = RxStreamingConfig(
-            receiveTimeOffset=0.1, noSamples=int(60e3)
-        )
-        system.configureTx(usrpName="usrp1", txStreamingConfig=txStreamingConfig1)
-        system.configureRx(usrpName="usrp1", rxStreamingConfig=rxStreamingConfig1)
-
-        system.configureRx(usrpName="usrp2", rxStreamingConfig=rxStreamingConfig2)
-
-        system.execute()
-        samplesSystem = system.collect()
-        rxSamplesUsrp1 = samplesSystem["usrp1"][0].signals[0]
-        rxSamplesUsrp2 = samplesSystem["usrp2"][0].signals[0]
-
-        self.assertAlmostEqual(
-            first=self.findSignalStartsInFrame(rxSamplesUsrp1, self.randomSignal),
-            second=300,
-            delta=10,
-        )
-        self.assertAlmostEqual(
-            first=self.findSignalStartsInFrame(rxSamplesUsrp2, self.randomSignal),
-            second=300,
-            delta=10,
-        )

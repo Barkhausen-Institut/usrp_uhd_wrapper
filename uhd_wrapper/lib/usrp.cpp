79ef93106f926137cb6973e1c0989d7f86154349
CoRoLa generic <corola@bi>
2022-05-18 15:05:41 +0200

    delete changes

--- uhd_wrapper/lib/usrp.cpp
+++ uhd_wrapper/lib/usrp.cpp
@@ -44,28 +44,22 @@ void Usrp::processRxStreamingConfig(const RxStreamingConfig &config,
     buffer = MimoSignal((size_t)rfConfig_.noRxAntennas,
                         samples_vec((size_t)config.noSamples, sample(0, 0)));
 
-    // size_t noPackages = calcNoPackages(config.noSamples, SAMPLES_PER_BUFFER);
-    // size_t noSamplesLastBuffer =
-    //    calcNoSamplesLastBuffer(config.noSamples, SAMPLES_PER_BUFFER);
-    double secondsInFuture = 0.25;
+    size_t noPackages = calcNoPackages(config.noSamples, SAMPLES_PER_BUFFER);
+    size_t noSamplesLastBuffer =
+        calcNoSamplesLastBuffer(config.noSamples, SAMPLES_PER_BUFFER);
     uhd::stream_cmd_t streamCmd =
         uhd::stream_cmd_t::STREAM_MODE_NUM_SAMPS_AND_DONE;
     streamCmd.num_samps = config.noSamples;
     streamCmd.stream_now = false;
-    // streamCmd.time_spec = uhd::time_spec_t(baseTime +
-    // config.receiveTimeOffset);
-    streamCmd.time_spec = secondsInFuture;
+    streamCmd.time_spec = uhd::time_spec_t(baseTime + config.receiveTimeOffset);
     rxStreamer_->issue_stream_cmd(streamCmd);
 
-    std::cout << "config.noSamples: " << config.noSamples << std::endl;
-    // std::cout << "noSamplesLastBuffer: " << noSamplesLastBuffer << std::endl;
     uhd::rx_metadata_t mdRx;
-    // double timeout =
-    //    (baseTime + config.receiveTimeOffset) - getCurrentFpgaTime() + 0.2;
-    double timeout = secondsInFuture + 0.1;
+    double timeout =
+        (baseTime + config.receiveTimeOffset) - getCurrentFpgaTime() + 0.2;
     size_t totalSamplesRecvd = 0;
     size_t remainingNoSamples = config.noSamples;
-    size_t noSamplesRcvd = rxStreamer_->get_max_num_samps();
+    size_t noMaxNumSamples = rxStreamer_->get_max_num_samps();
     while (totalSamplesRecvd < config.noSamples) {
         std::vector<sample *> buffers;
         for (int rxAntennaIdx = 0; rxAntennaIdx < rfConfig_.noRxAntennas;
@@ -73,16 +67,13 @@ void Usrp::processRxStreamingConfig(const RxStreamingConfig &config,
             buffers.push_back(buffer[rxAntennaIdx].data() + totalSamplesRecvd);
         }
         remainingNoSamples = config.noSamples - totalSamplesRecvd;
-        size_t noSamplesNextPkg = remainingNoSamples < noSamplesRcvd
+        size_t noSamplesNextPkg = remainingNoSamples < noMaxNumSamples
                                       ? remainingNoSamples
-                                      : noSamplesRcvd;
-        std::cout << "noSamplesNextPkg: " << noSamplesNextPkg << std::endl;
-        noSamplesRcvd =
+                                      : noMaxNumSamples;
+        size_t noSamplesRcvd =
             rxStreamer_->recv(buffers, noSamplesNextPkg, mdRx, timeout, true);
 
         totalSamplesRecvd += noSamplesRcvd;
-        std::cout << "New package received with " << noSamplesRcvd
-                  << ", in total we have " << totalSamplesRecvd << std::endl;
 
         timeout = 0.1f;
         if (mdRx.error_code !=
@@ -168,8 +159,8 @@ void Usrp::setRfConfig(const RfConfig &conf) {
     }
     usrpDevice_->set_time_now(uhd::time_spec_t(0.0));
     if (!subdevSpecSet_) {
-        // usrpDevice_->set_rx_subdev_spec(
-        //    uhd::usrp::subdev_spec_t(SUBDEV_SPECS[conf.noRxAntennas - 1]), 0);
+        usrpDevice_->set_rx_subdev_spec(
+            uhd::usrp::subdev_spec_t(SUBDEV_SPECS[conf.noRxAntennas - 1]), 0);
         usrpDevice_->set_tx_subdev_spec(uhd::usrp::subdev_spec_t("A:0"), 0);
         subdevSpecSet_ = true;
     }
@@ -181,9 +172,9 @@ void Usrp::setRfConfig(const RfConfig &conf) {
     }
     if (!rxStreamer_) {
         uhd::stream_args_t rxStreamArgs("fc32", "");
-        rxStreamArgs.channels = {0, 1};
-        // std::iota(rxStreamArgs.channels.begin(), rxStreamArgs.channels.end(),
-        //          0);
+        rxStreamArgs.channels = std::vector<size_t>(conf.noRxAntennas, 0);
+        std::iota(rxStreamArgs.channels.begin(), rxStreamArgs.channels.end(),
+                  0);
         rxStreamer_ = usrpDevice_->get_rx_stream(rxStreamArgs);
     }
 

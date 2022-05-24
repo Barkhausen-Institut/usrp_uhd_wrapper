5d30140c6d6715cfc2db3ed26754ca392492e0df
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-05-24 13:52:40 +0200

    check in setting rf config if mimo config changed and recreate usrp if so

--- uhd_wrapper/rpc_server/reconfigurable_usrp.py
+++ uhd_wrapper/rpc_server/reconfigurable_usrp.py
@@ -11,11 +11,12 @@ class RestartingUsrp:
     RestartTrials = 3
 
     def __init__(self, ip: str) -> None:
+        self._ip = ip
         startAttempt = 1
         usrpStarted = False
         while not usrpStarted and startAttempt <= self.RestartTrials:
             try:
-                self.__usrp = pybinding.createUsrp(ip)
+                self._usrp = pybinding.createUsrp(self._ip)
                 usrpStarted = True
             except RuntimeError:
                 print("Creation of USRP failed... Retrying after 2 seconds.")
@@ -26,34 +27,54 @@ class RestartingUsrp:
             sys.exit("Could not start USRP... exiting.")
 
     def setRfConfig(self, rfConfig: RfConfig) -> None:
-        self.__usrp.setRfConfig(rfConfig)
+        self._usrp.setRfConfig(rfConfig)
 
     def setRxConfig(self, rxConfig: RxStreamingConfig) -> None:
-        self.__usrp.setRxConfig(rxConfig)
+        self._usrp.setRxConfig(rxConfig)
 
     def setTxConfig(self, txConfig: TxStreamingConfig) -> None:
-        self.__usrp.setTxConfig(txConfig)
+        self._usrp.setTxConfig(txConfig)
 
     def setTimeToZeroNextPps(self) -> None:
-        self.__usrp.setTimeToZeroNextPps()
+        self._usrp.setTimeToZeroNextPps()
 
     def getCurrentSystemTime(self) -> int:
-        return self.__usrp.getCurrentSystemTime()
+        return self._usrp.getCurrentSystemTime()
 
     def getCurrentFpgaTime(self) -> int:
-        return self.__usrp.getCurrentFpgaTime()
+        return self._usrp.getCurrentFpgaTime()
 
     def execute(self, baseTime: float) -> None:
-        self.__usrp.execute(baseTime)
+        self._usrp.execute(baseTime)
 
     def collect(self) -> List[MimoSignal]:
-        return [MimoSignal(signals=c) for c in self.__usrp.collect()]
+        return [MimoSignal(signals=c) for c in self._usrp.collect()]
 
     def resetStreamingConfigs(self) -> None:
-        self.__usrp.resetStreamingConfigs()
+        self._usrp.resetStreamingConfigs()
 
     def getMasterClockRate(self) -> float:
-        return self.__usrp.getMasterClockRate()
+        return self._usrp.getMasterClockRate()
 
     def getRfConfig(self) -> RfConfig:
-        return self.__usrp.getRfConfig()
+        return self._usrp.getRfConfig()
+
+
+class MimoReconfiguringUsrp(RestartingUsrp):
+    def __init__(self, ip: str) -> None:
+        super().__init__(ip)
+        self._currentRfConfig: RfConfig = None
+
+    def setRfConfig(self, rfConfig: RfConfig) -> None:
+        if self.__mimoConfigChanged(rfConfig):
+            self._usrp = pybinding.createUsrp(self._ip)
+        self._usrp.setRfConfig(rfConfig)
+        self._currentRfConfig = rfConfig
+
+    def __mimoConfigChanged(self, newRfConfig: RfConfig) -> bool:
+        hasChanged = False
+        if self._currentRfConfig is not None:
+            hasChanged = (
+                self._currentRfConfig.noRxAntennas != newRfConfig.noRxAntennas
+            ) or (self._currentRfConfig.noTxAntennas != newRfConfig.noTxAntennas)
+        return hasChanged
---
+++ uhd_wrapper/tests/python/test_mimo_reconfiguring_usrp.py
@@ -0,0 +1,27 @@
+import unittest
+from unittest.mock import patch, Mock
+
+from uhd_wrapper.rpc_server.reconfigurable_usrp import MimoReconfiguringUsrp
+from uhd_wrapper.usrp_pybinding import RfConfig
+from uhd_wrapper.utils.config import fillDummyRfConfig
+
+
+class TestMimoReconfigUsrp(unittest.TestCase):
+    def setUp(self) -> None:
+        self.usrpIp = "localhost"
+        usrpFactoryPatcher = patch(
+            "uhd_wrapper.usrp_pybinding.createUsrp", return_value=Mock()
+        )
+        self.mockedUsrpFactoryFunction = usrpFactoryPatcher.start()
+        self.M = MimoReconfiguringUsrp(self.usrpIp)
+        self.mockedUsrpFactoryFunction.reset_mock()
+
+    def test_mimoConfigChanges_usrpRestarted(self) -> None:
+        sisoRfConfig = fillDummyRfConfig(RfConfig())
+        mimoRfConfig = fillDummyRfConfig(RfConfig())
+        mimoRfConfig.noRxAntennas = 2
+        mimoRfConfig.noTxAntennas = 4
+
+        self.M.setRfConfig(sisoRfConfig)
+        self.M.setRfConfig(mimoRfConfig)
+        self.mockedUsrpFactoryFunction.assert_called_once_with(self.usrpIp)

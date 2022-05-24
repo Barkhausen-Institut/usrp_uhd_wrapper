dde89cf1a23f084863bb3d2891a82ca6e129a2b5
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-05-24 11:44:09 +0200

    add unit tests for getting functions passed through

--- uhd_wrapper/tests/python/test_reconfigurable_usrp.py
+++ uhd_wrapper/tests/python/test_reconfigurable_usrp.py
@@ -1,10 +1,16 @@
+from email.mime import base
 import unittest
 from unittest.mock import Mock, patch
 
 import numpy as np
 
 from uhd_wrapper.rpc_server.reconfigurable_usrp import ReconfigurableUsrp
-from uhd_wrapper.usrp_pybinding import Usrp, RfConfig, TxStreamingConfig
+from uhd_wrapper.usrp_pybinding import (
+    Usrp,
+    RfConfig,
+    TxStreamingConfig,
+    RxStreamingConfig,
+)
 from uhd_wrapper.utils.config import fillDummyRfConfig
 
 
@@ -41,6 +47,7 @@ class TestUsrpStarts(unittest.TestCase):
 class TestFunctionsGetPassedThrough(unittest.TestCase):
     def setUp(self) -> None:
         self.mockedUsrpDevice = Mock(spec=Usrp)
+        self.mockedUsrpDevice.collect.return_value = [[np.ones(int(10e3))]]
         usrpFactoryPatcher = patch(
             "uhd_wrapper.usrp_pybinding.createUsrp", return_value=self.mockedUsrpDevice
         )
@@ -62,3 +69,45 @@ class TestFunctionsGetPassedThrough(unittest.TestCase):
         self.mockedUsrpDevice.setTxConfig.assert_called_once_with(
             dummyTxStreamingConfig
         )
+
+    def test_rxConfigGetsPassedThrough(self) -> None:
+        dummyRxStreamingConfig = RxStreamingConfig(
+            noSamples=int(1e3), receiveTimeOffset=3.0
+        )
+        self.R.setRxConfig(dummyRxStreamingConfig)
+        self.mockedUsrpDevice.setRxConfig.assert_called_once_with(
+            dummyRxStreamingConfig
+        )
+
+    def test_setTimeToZeroNextPpps(self) -> None:
+        self.R.setTimeToZeroNextPps()
+        self.mockedUsrpDevice.setTimeToZeroNextPps.assert_called_once()
+
+    def test_getCurrentSystemTime(self) -> None:
+        _ = self.R.getCurrentSystemTime()
+        self.mockedUsrpDevice.getCurrentSystemTime.assert_called_once()
+
+    def test_getCurrentFpgaTime(self) -> None:
+        _ = self.R.getCurrentFpgaTime()
+        self.mockedUsrpDevice.getCurrentFpgaTime.assert_called_once()
+
+    def test_execute(self) -> None:
+        baseTime = 1.0
+        self.R.execute(baseTime)
+        self.mockedUsrpDevice.execute.assert_called_once_with(baseTime)
+
+    def test_collect(self) -> None:
+        _ = self.R.collect()
+        self.mockedUsrpDevice.collect.assert_called_once()
+
+    def test_resetStreamingConfigs(self) -> None:
+        self.R.resetStreamingConfigs()
+        self.mockedUsrpDevice.resetStreamingConfigs.assert_called_once()
+
+    def test_getMasterClockRate(self) -> None:
+        _ = self.R.getMasterClockRate()
+        self.mockedUsrpDevice.getMasterClockRate.assert_called_once()
+
+    def test_getRfConfig(self) -> None:
+        _ = self.R.getRfConfig()
+        self.mockedUsrpDevice.getRfConfig.assert_called_once()

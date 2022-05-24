ac882bfd5bdbd7a1459ca5ff262786aacd31f7c5
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-05-24 11:18:05 +0200

    add test if usrp is created after first attempt

--- uhd_wrapper/tests/python/test_reconfigurable_usrp.py
+++ uhd_wrapper/tests/python/test_reconfigurable_usrp.py
@@ -8,8 +8,18 @@ from uhd_wrapper.usrp_pybinding import Usrp, createUsrp
 
 
 class TestReconfigurableUsrp(unittest.TestCase):
+    def setUp(self) -> None:
+        sleepPatcher = patch("time.sleep", return_value=None)
+        _ = sleepPatcher.start()
+
     @patch("uhd_wrapper.usrp_pybinding.createUsrp")
     def test_initCreatesUsrp(self, mockedUsrpFactoryFunction) -> None:
         IP = "myIp"
         _ = ReconfigurableUsrp(IP)
         mockedUsrpFactoryFunction.assert_called_once_with(IP)
+
+    @patch("uhd_wrapper.usrp_pybinding.createUsrp")
+    def test_usrpCreatedAfterFirstFail(self, mockedUsrpFactoryFunction) -> None:
+        mockedUsrpFactoryFunction.side_effect = [RuntimeError(), Mock(), Mock()]
+        _ = ReconfigurableUsrp("localhost")
+        self.assertEqual(mockedUsrpFactoryFunction.call_count, 2)

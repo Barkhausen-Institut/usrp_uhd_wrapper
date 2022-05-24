c144a632764e00cf641d5ab42c963fe285c1e19e
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-05-24 13:07:24 +0200

    use reconfigurableUsrp for usrp

--- start_usrp_server.py
+++ start_usrp_server.py
@@ -1,11 +1,11 @@
 from uhd_wrapper.rpc_server.rpc_server import UsrpServer
 import zerorpc
-from uhd_wrapper.usrp_pybinding import createUsrp
+from uhd_wrapper.rpc_server.reconfigurable_usrp import ReconfigurableUsrp
 
 # create environment
 PORT = 5555
-IP_USRP = "localhost"
-usrp = createUsrp(IP_USRP)
+IP_USRP = "192.168.189.155"
+usrp = ReconfigurableUsrp(IP_USRP)
 
 # start server
 rpcServer = zerorpc.Server(UsrpServer(usrp))

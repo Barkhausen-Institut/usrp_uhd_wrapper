f95acafcb74f64c3f0065fc49ca4f9f0e4ffb3ab
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-05-25 11:31:49 +0000

    start service only after 60s uptime

--- start_usrp_server.bash
+++ start_usrp_server.bash
@@ -1,6 +1,10 @@
 export XDG_CONFIG_HOME=/home/root/
 export HOME=/home/root/
 export UHD_LOG_FILE=usrp_uhd_log.txt
+while [ "$(cat /proc/uptime | cut -f 1 -d '.')" -le 60 ]
+do
+       sleep 1	
+done
 cd /home/root/usrp_uhd_api/
 . env/bin/activate
 python start_usrp_server.py

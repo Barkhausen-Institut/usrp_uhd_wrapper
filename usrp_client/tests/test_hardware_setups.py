8f7452080c86e5e3cf879819590c3b697251cce5
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-05-25 13:50:33 +0200

    add reuse system tests for rx mimo

--- usrp_client/tests/test_hardware_setups.py
+++ usrp_client/tests/test_hardware_setups.py
@@ -99,39 +99,41 @@ class TestHardwareSystemTests(unittest.TestCase):
             + 1j * np.random.sample((self.noSamples,))
         ) - (0.5 + 0.5j)
 
-    def test_oneTxAntennaFourRxAntennas_localhost(self) -> None:
+    def test_reUseSystemTenTimes_oneTxAntennaFourRxAntennas_localhost(self) -> None:
         setup = LocalTransmissionHardwareSetup()
         system = setup.connectUsrps()
-        rxStreamingConfig1 = RxStreamingConfig(
-            receiveTimeOffset=0.0, noSamples=int(60e3)
-        )
-        txStreamingConfig1 = TxStreamingConfig(
-            sendTimeOffset=0.0, samples=MimoSignal(signals=[self.randomSignal])
-        )
-        system.configureRx(usrpName="usrp1", rxStreamingConfig=rxStreamingConfig1)
-        system.configureTx(usrpName="usrp1", txStreamingConfig=txStreamingConfig1)
-        system.execute()
-        samplesSystem = system.collect()
-        rxSamplesUsrpAnt1 = samplesSystem["usrp1"][0].signals[0]
-        rxSamplesUsrpAnt2 = samplesSystem["usrp1"][0].signals[1]
-        rxSamplesUsrpAnt3 = samplesSystem["usrp1"][0].signals[2]
-        rxSamplesUsrpAnt4 = samplesSystem["usrp1"][0].signals[3]
 
-        self.assertEqual(
-            first=findSignalStartsInFrame(rxSamplesUsrpAnt1, self.randomSignal),
-            second=findSignalStartsInFrame(rxSamplesUsrpAnt2, self.randomSignal),
-        )
-        self.assertEqual(
-            first=findSignalStartsInFrame(rxSamplesUsrpAnt1, self.randomSignal),
-            second=findSignalStartsInFrame(rxSamplesUsrpAnt3, self.randomSignal),
-        )
-        self.assertEqual(
-            first=findSignalStartsInFrame(rxSamplesUsrpAnt1, self.randomSignal),
-            second=findSignalStartsInFrame(rxSamplesUsrpAnt4, self.randomSignal),
-        )
-        self.assertGreater(np.sum(np.abs(rxSamplesUsrpAnt1 - rxSamplesUsrpAnt2)), 1)
-        self.assertGreater(np.sum(np.abs(rxSamplesUsrpAnt1 - rxSamplesUsrpAnt3)), 1)
-        self.assertGreater(np.sum(np.abs(rxSamplesUsrpAnt1 - rxSamplesUsrpAnt4)), 1)
+        for _ in range(10):
+            rxStreamingConfig1 = RxStreamingConfig(
+                receiveTimeOffset=0.0, noSamples=int(60e3)
+            )
+            txStreamingConfig1 = TxStreamingConfig(
+                sendTimeOffset=0.0, samples=MimoSignal(signals=[self.randomSignal])
+            )
+            system.configureRx(usrpName="usrp1", rxStreamingConfig=rxStreamingConfig1)
+            system.configureTx(usrpName="usrp1", txStreamingConfig=txStreamingConfig1)
+            system.execute()
+            samplesSystem = system.collect()
+            rxSamplesUsrpAnt1 = samplesSystem["usrp1"][0].signals[0]
+            rxSamplesUsrpAnt2 = samplesSystem["usrp1"][0].signals[1]
+            rxSamplesUsrpAnt3 = samplesSystem["usrp1"][0].signals[2]
+            rxSamplesUsrpAnt4 = samplesSystem["usrp1"][0].signals[3]
+
+            self.assertEqual(
+                first=findSignalStartsInFrame(rxSamplesUsrpAnt1, self.randomSignal),
+                second=findSignalStartsInFrame(rxSamplesUsrpAnt2, self.randomSignal),
+            )
+            self.assertEqual(
+                first=findSignalStartsInFrame(rxSamplesUsrpAnt1, self.randomSignal),
+                second=findSignalStartsInFrame(rxSamplesUsrpAnt3, self.randomSignal),
+            )
+            self.assertEqual(
+                first=findSignalStartsInFrame(rxSamplesUsrpAnt1, self.randomSignal),
+                second=findSignalStartsInFrame(rxSamplesUsrpAnt4, self.randomSignal),
+            )
+            self.assertGreater(np.sum(np.abs(rxSamplesUsrpAnt1 - rxSamplesUsrpAnt2)), 1)
+            self.assertGreater(np.sum(np.abs(rxSamplesUsrpAnt1 - rxSamplesUsrpAnt3)), 1)
+            self.assertGreater(np.sum(np.abs(rxSamplesUsrpAnt1 - rxSamplesUsrpAnt4)), 1)
 
     def test_p2pTransmission(self) -> None:
         setup = P2pHardwareSetup()

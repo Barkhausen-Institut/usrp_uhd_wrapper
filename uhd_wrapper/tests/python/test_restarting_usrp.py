05af459da4e538ad12b6b8901b8d36bdc4985da6
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2022-06-10 09:51:59 +0200

    stop patcher

--- uhd_wrapper/tests/python/test_restarting_usrp.py
+++ uhd_wrapper/tests/python/test_restarting_usrp.py
@@ -2,19 +2,23 @@ import unittest
 from unittest.mock import Mock, patch
 
 from uhd_wrapper.rpc_server.reconfigurable_usrp import RestartingUsrp
+from uhd_wrapper.usrp_pybinding import Usrp
 
 
 class TestUsrpStarts(unittest.TestCase):
     def setUp(self) -> None:
         self.sleepPatcher = patch("time.sleep", return_value=None)
         _ = self.sleepPatcher.start()
-        usrpFactoryPatcher = patch(
-            "uhd_wrapper.usrp_pybinding.createUsrp", return_value=Mock()
+
+        self.usrpMock = Mock(spec=Usrp)
+        self.usrpFactoryPatcher = patch(
+            "uhd_wrapper.usrp_pybinding.createUsrp", return_value=self.usrpMock
         )
-        self.mockedUsrpFactoryFunction = usrpFactoryPatcher.start()
+        self.mockedUsrpFactoryFunction = self.usrpFactoryPatcher.start()
 
     def tearDown(self) -> None:
         self.sleepPatcher.stop()
+        self.usrpFactoryPatcher.stop()
 
     def test_initCreatesUsrp(self) -> None:
         IP = "myIp"

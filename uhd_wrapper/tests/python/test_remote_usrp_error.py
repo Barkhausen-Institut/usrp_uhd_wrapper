cadfee9cdf5c030ee21b1b906f3072d9df313a98
tokr-bit <tobias.kronauer@posteo.de>
2022-06-15 14:25:04 +0200

    rename AnnotatedUsrpException to RemoteUsrpError

--- uhd_wrapper/tests/python/test_remote_usrp_error.py
+++ uhd_wrapper/tests/python/test_remote_usrp_error.py
@@ -1,27 +1,27 @@
 import unittest
 
-from uhd_wrapper.utils.annotated_usrp_exception import AnnotatedUsrpException
+from uhd_wrapper.utils.remote_usrp_error import RemoteUsrpError
 
 
 def rethrowException_emptyUsrpField(msg: str) -> None:
     try:
         raise Exception(msg)
     except Exception as e:
-        raise AnnotatedUsrpException(str(e))
+        raise RemoteUsrpError(str(e))
 
 
 def rethrowException_setUsrpField(msg: str, usrpName: str) -> None:
     try:
         raise Exception(msg)
     except Exception as e:
-        raise AnnotatedUsrpException(str(e), usrpName)
+        raise RemoteUsrpError(str(e), usrpName)
 
 
-class TestAnnotatedUsrpException(unittest.TestCase):
+class TestRemoteUsrpError(unittest.TestCase):
     def test_standardExceptionContainsEmptyUsrpField(self) -> None:
         try:
             rethrowException_emptyUsrpField("foo")
-        except AnnotatedUsrpException as e:
+        except RemoteUsrpError as e:
             self.assertEqual(e.usrpName, "")
 
     def test_msgCreatedWithUsrpExceptionMsg(self) -> None:
@@ -29,12 +29,12 @@ class TestAnnotatedUsrpException(unittest.TestCase):
         USRP_NAME = "usrp1"
         try:
             rethrowException_setUsrpField(DUMMY_MSG, USRP_NAME)
-        except AnnotatedUsrpException as e:
+        except RemoteUsrpError as e:
             self.assertIn(USRP_NAME, str(e))
 
     def test_msgContainsMsgOfReraisedException(self) -> None:
         DUMMY_MSG = "foo"
         try:
             rethrowException_setUsrpField(DUMMY_MSG, "usrp1")
-        except AnnotatedUsrpException as e:
+        except RemoteUsrpError as e:
             self.assertIn(DUMMY_MSG, str(e))

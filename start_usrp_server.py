87ec5d3b0fd2a287d134bb43e5add443afb31d0e
CoRoLa generic <corola@bi>
2023-01-03 15:20:38 +0100

    allow USRP RPC server to configure the target UHD IP

--- start_usrp_server.py
+++ start_usrp_server.py
@@ -1,10 +1,18 @@
+import argparse
+
 from uhd_wrapper.rpc_server.rpc_server import UsrpServer
 import zerorpc
 from uhd_wrapper.rpc_server.reconfigurable_usrp import MimoReconfiguringUsrp
 
+def parseArgs():
+    parser = argparse.ArgumentParser(description="Start the USRP RPC server")
+    parser.add_argument("--uhd-ip", type=str, default="localhost", help="Determine the IP of the USRP to connect to")
+
+    return parser.parse_args()
+
 # create environment
 PORT = 5555
-IP_USRP = "localhost"
+IP_USRP = parseArgs().uhd_ip
 usrp = MimoReconfiguringUsrp(IP_USRP)
 
 # start server

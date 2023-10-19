e1fbcd903a7217058675646efafc0a0e44e5d715
Maximilian Matthe <maxi.matthe@googlemail.com>
2023-10-19 14:57:12 +0200

    fix linting ofdm.py

--- examples/ofdm.py
+++ examples/ofdm.py
@@ -1,6 +1,7 @@
-# This example runs a single OFDM frame without CP and wtih no channel estimation/equalization.
-# It shows the unequalized constellation at the RX side, along with the channel phase and amplitude.
-# Parameters to adjust: Fs, Fc, delay, N, Non, ip
+# This example runs a single OFDM frame without CP and wtih no channel
+# estimation/equalization. It shows the unequalized constellation at the RX
+# side, along with the channel phase and amplitude. Parameters to adjust: Fs,
+# Fc, delay, N, Non, ip
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -40,7 +41,7 @@ ip = "192.168.199.134"  # IP of the USRP to attach to
 Fc = 3.7e9   # Carrier frequenc
 Fs = 2*245.76e6   # Sampling rate
 
-delay =363        # Delay of RX towards TX in samples. Get e.g. by running
+delay = 363        # Delay of RX towards TX in samples. Get e.g. by running
 # python -m usrp_client.sanity --ips 192.168.199.134  --single  --sync --fs 491.52e6 --fc 3.7e9
 
 padding = 1000  # How many zeros to append to the beginning of the frame to let RF ramp up
@@ -49,10 +50,10 @@ N = 2048  # Number of subcarriers
 Non = 1200   # Number of switched-on subcarriers
 Noff = N - Non
 
-I = 1 - 2 * (np.random.randn(N) > 0)
-Q = 1 - 2 * (np.random.randn(N) > 0)
+II = 1 - 2 * (np.random.randn(N) > 0)
+QQ = 1 - 2 * (np.random.randn(N) > 0)
 
-IQ = I + 1j*Q
+IQ = II + 1j*QQ
 IQ[:Noff//2] = 0
 IQ[-Noff//2:] = 0
 
@@ -64,8 +65,9 @@ txSig = txSig / max(abs(txSig))
 
 
 system = createSystem(fc=Fc, fs=Fs, txGain=30, rxGain=30, ipUsrp1=ip)
-system.configureTx(usrpName="usrp1", txStreamingConfig=TxStreamingConfig(samples=MimoSignal(signals=[txSig]),
-                                                                         sendTimeOffset=0.0))
+system.configureTx(usrpName="usrp1",
+                   txStreamingConfig=TxStreamingConfig(samples=MimoSignal(signals=[txSig]),
+                                                       sendTimeOffset=0.0))
 system.configureRx(usrpName="usrp1", rxStreamingConfig=RxStreamingConfig(
     noSamples=len(txSig), receiveTimeOffset=0.0))
 
